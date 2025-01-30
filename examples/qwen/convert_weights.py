bbb93d5d6bd87003a7cf023d2318f986ae401f59
Shaipranesh S <80086120+shaipranesh2@users.noreply.github.com>
2025-01-30 22:01:22 +0530

    removing non-qwen elements

--- examples/qwen/convert_weights.py
+++ examples/qwen/convert_weights.py
@@ -69,7 +69,6 @@ def get_config_mapping(nt_to_hf: bool = True) -> dict[str, str]:
         "num_key_value_heads": "num_key_value_heads",
         "pad_token_id": "pad_token_id",
         "rms_norm_eps": "rms_norm_eps",
-        "rope_scaling": "rope_scaling",
         "rope_theta": "rope_theta",
         "tie_word_embeddings": "tie_word_embeddings",
         "use_cache": "use_cache",
